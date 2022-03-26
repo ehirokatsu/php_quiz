cc24043d9b98493be4e3c1faa790d839feb42649
ehirokatsu <48009332+ehirokatsu@users.noreply.github.com>
2022-03-26 22:18:52 +0900

    Add files via upload

--- Quiz.php
+++ Quiz.php
@@ -7,45 +7,51 @@
 <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+
+
+
 <br>
 <br>
 <div class="center">
-<form action="/Quiz_Answer.php" method="post">
-    <!-- フォームの部品 -->
-        <div>
-            <label for="quiz1">クイズ１：メールを送信するプロトコルは何でしょうか？</label>
-            <br>
-            <input type="text" id="quiz1" name="quiz1">
-        </div>
+<H1>
+<font color="blue">クイズ課題</font>
+
+</H1>
+
+<form action="/Quiz_Answer.php" method="post" onsubmit="return check()">
+    <div>
+        <label for="quiz1">クイズ１：メールを送信するプロトコルは何でしょうか？</label>
         <br>
-        <div>
-            <label for="quiz2">クイズ２：IPヘッダに存在する項目はどれでしょうか？</label>
-            <br>
-            <input type="checkbox" name="quiz2[]" value="IPアドレス">
-            <label for="quiz2_1">IPアドレス</label>
-            <br>
-            <input type="checkbox" name="quiz2[]" value="ポート番号">
-            <label for="quiz2_2">ポート番号</label>
-            <br>
-            <input type="checkbox" name="quiz2[]" value="TTL">
-            <label for="quiz2_3">TTL</label>
-            <br>
-        </div>
+        <input type="text" id="quiz1" name="quiz1">
+    </div>
+    <br>
+    <div>
+        <label for="quiz2">クイズ２：IPヘッダに存在する項目はどれでしょうか？</label>
         <br>
-        <div>
-            <label for="quiz3">クイズ３：正しいIPアドレスはどれでしょうか？</label>
-            <br>
-            <input type="radio" name="quiz3" value="192.168.255.256"> 192.168.255.256
-            <br>
-            <input type="radio" name="quiz3" value="192.168.1.255"> 192.168.1.255
-            <br>
-            <input type="radio" name="quiz3" value="192.168.0.-1"> 192.168.0.-1
-            <br>
-            <br>
-        </div>
-        <input type="submit" value="送信する">
+        <input type="checkbox" id="check1" name="quiz2[]" value="IPアドレス">
+        <label for="check1">IPアドレス</label>
+        <input type="checkbox" id="check2" name="quiz2[]" value="ポート番号">
+        <label for="check2">ポート番号</label>
+        <input type="checkbox" id="check3" name="quiz2[]" value="TTL">
+        <label for="check3">TTL</label>
         <br>
-        
+    </div>
+    <br>
+    <div>
+        <label for="quiz3">クイズ３：正しいIPアドレスはどれでしょうか？</label>
+        <br>
+        <input type="radio" id="radio1" name="quiz3" value="192.168.255.256">
+        <label for="radio1">192.168.255.256</label>
+        <input type="radio" id="radio2" name="quiz3" value="192.168.1.255">
+        <label for="radio2">192.168.1.255</label>
+        <input type="radio" id="radio3" name="quiz3" value="192.168.0.-1">
+        <label for="radio3">192.168.0.-1</label>
+        <br>
+        <br>
+    </div>
+    <input type="submit" value="送信する">
+    <br>
+    
 </form>
 
 </div>
--- Quiz_Answer.php
+++ Quiz_Answer.php
@@ -7,37 +7,81 @@
 <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+
+<br>
+<br>
+<div class="center">
+<H1>
+<font color="blue">クイズ課題 結果</font>
+
+</H1>
 <?php
 
-$quiz1 = $_POST['quiz1'];
-$quiz2 = $_POST['quiz2'];
-$quiz3 = $_POST['quiz3'];
 
-//var_dump($quiz3);
+
 
 $quiz1_Ans="SMTP";
 $quiz3_Ans="192.168.1.255";
 
-if (strcmp($quiz1_Ans, $quiz1) == 0) {
-echo "OK";
+
+
+echo "クイズ１：回答→";
+if (isset($_POST['quiz1']) && strcmp("", $_POST['quiz1'])) {
+    echo $quiz1;
+    if (strcmp($quiz1_Ans, $_POST['quiz1']) === 0) {
+        echo " OK";
+    } else {
+        echo " NG";
+    }
 } else {
-echo "NG";
+?>
+    <font color="red">   回答が入力されていません！</font>
+    <br>
+    <br>
+<?php
 }
+//var_dump($_POST['quiz2']);
+echo "クイズ２：回答→";
+if (isset($_POST['quiz2']) && is_array($_POST['quiz2'])) {
+
+    foreach($_POST['quiz2'] as $value){
+       echo $value;
+       echo "\n";
+    }
+    if (in_array("IPアドレス", $_POST['quiz2'])
+        && !in_array("ポート番号", $_POST['quiz2'])
+        && in_array("TTL", $_POST['quiz2'])) {
+        echo " OK";
+    } else {
+        echo " NG";
+    }
+} else {
+?>
+    <font color="red">   回答が入力されていません！</font>
+<?php
+}
+
 
 if (in_array("IPアドレス", $quiz2) && !in_array("ポート番号", $quiz2) && in_array("TTL", $quiz2)) {
-echo "OK";
+echo "quiz2OK";
 } else {
-echo "NG";
+echo "quiz2NG";
 }
 
+
 if (strcmp($quiz3_Ans, $quiz3) == 0) {
-echo "OK";
+echo "quiz3OK";
 } else {
-echo "NG";
+echo "quiz3NG";
 }
 
 
 ?>
+
+<a href="Quiz.php">戻る</a>
+<br>
+
+</div>
 
 
 </body>
