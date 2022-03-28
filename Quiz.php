3e0680b6d0ac762e9cc3eeee8cdb2e364b349df3
ehirokatsu <48009332+ehirokatsu@users.noreply.github.com>
2022-03-28 15:46:23 +0900

    Add files via upload

--- Quiz.php
+++ Quiz.php
@@ -3,13 +3,10 @@
 <html xmlns="http:// www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
-<title>PHP Calendar</title>
+<title>PHP Quiz</title>
 <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-
-
-
 <br>
 <br>
 <div class="center">
@@ -19,39 +16,33 @@
 </H1>
 
 <form action="/Quiz_Answer.php" method="post" onsubmit="return check()">
-    <div>
-        <label for="quiz1">クイズ１：メールを送信するプロトコルは何でしょうか？</label>
-        <br>
-        <input type="text" id="quiz1" name="quiz1">
-    </div>
+    <label for="quiz1">クイズ１：WEBページを記述するための言語は何でしょうか？</label>
     <br>
-    <div>
-        <label for="quiz2">クイズ２：IPヘッダに存在する項目はどれでしょうか？</label>
-        <br>
-        <input type="checkbox" id="check1" name="quiz2[]" value="IPアドレス">
-        <label for="check1">IPアドレス</label>
-        <input type="checkbox" id="check2" name="quiz2[]" value="ポート番号">
-        <label for="check2">ポート番号</label>
-        <input type="checkbox" id="check3" name="quiz2[]" value="TTL">
-        <label for="check3">TTL</label>
-        <br>
-    </div>
+    <input type="text" id="quiz1" name="quiz1">
     <br>
-    <div>
-        <label for="quiz3">クイズ３：正しいIPアドレスはどれでしょうか？</label>
-        <br>
-        <input type="radio" id="radio1" name="quiz3" value="192.168.255.256">
-        <label for="radio1">192.168.255.256</label>
-        <input type="radio" id="radio2" name="quiz3" value="192.168.1.255">
-        <label for="radio2">192.168.1.255</label>
-        <input type="radio" id="radio3" name="quiz3" value="192.168.0.-1">
-        <label for="radio3">192.168.0.-1</label>
-        <br>
-        <br>
-    </div>
+    <br>
+    <label for="quiz2">クイズ２：WEBサーバに使用されるものはどれでしょうか？</label>
+    <br>
+    <input type="checkbox" id="check1" name="quiz2[]" value="Apache">
+    <label for="check1">Apache</label>
+    <input type="checkbox" id="check2" name="quiz2[]" value="iPhone">
+    <label for="check2">iPhone</label>
+    <input type="checkbox" id="check3" name="quiz2[]" value="Tomcat">
+    <label for="check3">Tomcat</label>
+    <br>
+    <br>
+    <label for="quiz3">クイズ３：次のうちブラウザはどれでしょうか？</label>
+    <br>
+    <input type="radio" id="radio1" name="quiz3" value="Edge">
+    <label for="radio1">Edge</label>
+    <input type="radio" id="radio2" name="quiz3" value="Android">
+    <label for="radio2">Android</label>
+    <input type="radio" id="radio3" name="quiz3" value="Apple">
+    <label for="radio3">Apple</label>
+    <br>
+    <br>
     <input type="submit" value="送信する">
     <br>
-    
 </form>
 
 </div>
--- Quiz_Answer.php
+++ Quiz_Answer.php
@@ -3,7 +3,7 @@
 <html xmlns="http:// www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
-<title>PHP Calendar</title>
+<title>PHP Quiz Answer</title>
 <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
@@ -17,72 +17,101 @@
 </H1>
 <?php
 
-
-
-
-$quiz1_Ans="SMTP";
-$quiz3_Ans="192.168.1.255";
-
-
-
-echo "クイズ１：回答→";
-if (isset($_POST['quiz1']) && strcmp("", $_POST['quiz1'])) {
-    echo $quiz1;
-    if (strcmp($quiz1_Ans, $_POST['quiz1']) === 0) {
-        echo " OK";
-    } else {
-        echo " NG";
+    //表示する時のエスケープ処理関数
+    function checkEscape($str)
+    {
+        return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
     }
-} else {
-?>
-    <font color="red">   回答が入力されていません！</font>
-    <br>
-    <br>
-<?php
-}
-//var_dump($_POST['quiz2']);
-echo "クイズ２：回答→";
-if (isset($_POST['quiz2']) && is_array($_POST['quiz2'])) {
-
-    foreach($_POST['quiz2'] as $value){
-       echo $value;
-       echo "\n";
+    
+    //入力チェック関数(文字列用)
+    function checkString($str)
+    {
+        //戻り値用
+        $result = false;
+        
+        if ($str === '') {
+            echo '<font color="red">   回答が入力されていません！</font><br><br>';
+        } elseif (!is_string($str)) {
+            echo '<font color="red">   回答が不正送信されました！</font><br><br>';
+        } else {
+            $result = true;
+        }
+        return $result;
     }
-    if (in_array("IPアドレス", $_POST['quiz2'])
-        && !in_array("ポート番号", $_POST['quiz2'])
-        && in_array("TTL", $_POST['quiz2'])) {
-        echo " OK";
-    } else {
-        echo " NG";
+
+    //入力チェック関数(配列用)
+    function checkArray($array)
+    {
+        //戻り値用
+        $result = false;
+        //配列の不正判定用
+        $frag = false;
+
+        if (!is_array($array)) {
+            echo '<font color="red">   回答が入力されていません！</font><br><br>';
+        } else {
+            foreach ($array as $value) {
+                if (!is_string($value)) {
+                    echo '<font color="red">   回答が不正送信されました！</font><br><br>';
+                    $frag = true;
+                }
+            }
+            if (!$frag) {
+                $result = true;
+            }
+        }
+        return $result;
     }
-} else {
-?>
-    <font color="red">   回答が入力されていません！</font>
-<?php
-}
+    
+    //クイズ１の回答チェック
+    echo "クイズ１：回答→";
+    //quiz1がnullの場合、checkString($_POST['quiz1'])を呼び出すとundefined array keyエラーが発生する
+    if (!isset($_POST['quiz1'])) {
+        echo '<font color="red">   回答が送信されていません！</font><br><br>';
+    } elseif (checkString($_POST['quiz1'])) {
+        echo "[",checkEscape($_POST['quiz1']),"]";
+        if (strcmp("HTML", $_POST['quiz1']) === 0) {
+            echo '<font color="green">   正解！</font><br><br>';
+        } else {
+            echo '<font color="red">  不正解！</font><br><br>';
+        }
+    }
 
+    //クイズ２の回答チェック
+    echo "クイズ２：回答→";
+    if (!isset($_POST['quiz2'])) {
+        echo '<font color="red">   回答が送信されていません！</font><br><br>';
+    } elseif (checkArray($_POST['quiz2'])) {
+        foreach($_POST['quiz2'] as $value){
+           echo "[",checkEscape($value),"]","\n";
+        }
+        if (in_array("Apache", $_POST['quiz2'])
+            && !in_array("iPhone", $_POST['quiz2'])
+            && in_array("Tomcat", $_POST['quiz2'])) {
+                echo '<font color="green">   正解！</font><br><br>';
+        } else {
+            echo '<font color="red">  不正解！</font><br><br>';
+        }
+    }
 
-if (in_array("IPアドレス", $quiz2) && !in_array("ポート番号", $quiz2) && in_array("TTL", $quiz2)) {
-echo "quiz2OK";
-} else {
-echo "quiz2NG";
-}
-
-
-if (strcmp($quiz3_Ans, $quiz3) == 0) {
-echo "quiz3OK";
-} else {
-echo "quiz3NG";
-}
-
-
+    //クイズ３の回答チェック
+    echo "クイズ３：回答→";
+    if (!isset($_POST['quiz3'])) {
+        echo '<font color="red">   回答が送信されていません！</font><br><br>';
+    } elseif (checkString($_POST['quiz3'])) {
+        echo "[",checkEscape($_POST['quiz3']),"]";
+        if (strcmp("Edge", $_POST['quiz3']) === 0) {
+            echo '<font color="green">   正解！</font><br><br>';
+        } else {
+            echo '<font color="red">  不正解！</font><br><br>';
+        }
+    }
 ?>
 
 <a href="Quiz.php">戻る</a>
 <br>
 
 </div>
-
 
 </body>
 </html>
